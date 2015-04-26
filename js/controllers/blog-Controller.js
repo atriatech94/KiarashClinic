7d0850aa65fca189ecaea91930d0681a2df104c3
atriatech94 <hamid_7555@hotmail.com>
2015-04-26 15:23:23 +0430

    n37

--- js/controllers/blog-Controller.js
+++ js/controllers/blog-Controller.js
@@ -57,7 +57,7 @@ angular.module('kiarash')
                                                            $('.post_bl .setun2').append(result);
                                                    });
                                         });// end success
-                               
+								}
                               /*===============================================================*/ 
                                     
                               
