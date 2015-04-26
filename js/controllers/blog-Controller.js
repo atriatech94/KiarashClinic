1ed3e5cf3384ed5e40f1e2ca9b9339bdb0d6ff2b
atriatech94 <hamid_7555@hotmail.com>
2015-04-26 16:32:02 +0430

    n40

--- js/controllers/blog-Controller.js
+++ js/controllers/blog-Controller.js
@@ -34,10 +34,10 @@ angular.module('kiarash')
                                      /*=======================================================================*/
                                 }// $rootScope.datt == 1
                                 else{
-									  if(online == 0 &&  $rootScope.news == null){
-                            $.fancybox.open("<p>برای مشاهده این قسمت اینترنت گوشی خود را فعال کنید </p><button class='refresh'>تلاش مجدد</button>");
+							if(online == 0){
+                              $.fancybox.open("<p>برای مشاهده این قسمت اینترنت گوشی خود را فعال کنید </p><button class='refresh'>تلاش مجدد</button>");
                         }
-						else{
+						   else{
                                      /*===============================================================*/ 
                                        $('.loading_img').show(0);
                                         News.all()
