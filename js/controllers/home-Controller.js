309684c2f68779a85a277ce6bcfdbe5aa0947778
atriatech94 <hamid_7555@hotmail.com>
2015-05-06 19:53:58 +0430

    n65

--- js/controllers/home-Controller.js
+++ js/controllers/home-Controller.js
@@ -1,6 +1,6 @@
 angular.module('kiarash')
 .controller('HomeController', function($scope,$rootScope,News) {
-    
+
     var online = navigator.onLine;
     if(online){
         News.all()
@@ -14,6 +14,57 @@ angular.module('kiarash')
          $rootScope.datt= 0;
     }
 
-});
 
+})
+.directive('swSwipe', [ '$location', function ($location){
+		return {
+			link: function($scope) {
+				
+			    swiper2 = new Swiper('.swiper-container',{
+						   pagination: '.swiper-pagination',
+						   slidesPerView: 'auto',
+						   followFinger : false,      	
+					 });  
+				  
+				$(window).on('hashchange', function(e){
+					  
+						   swiper2 = new Swiper('.swiper-container',{
+						   pagination: '.swiper-pagination',
+						   slidesPerView: 'auto',
+						   followFinger : false,
+					   });
+					 
+                   
+				});
+				
+               $('body').delegate(".header_page .fir","click",function(){
+				     var flags = swiper2.progress;
+                if(flags==0){swiper2.slideNext(); }else{swiper2.slidePrev();}
+                });
+				
+				
+                /*========================================*/
+                $(window).on('hashchange', function(e){
+                   var loc =  $location.path();
+                   loc = loc.replace("/", "");
+                   $('.navbar li a').removeClass('active');
+                   $('#'+loc).children("a").addClass('active');
+				 
+		
+				 
+                });
+                
+                /*===========================================================*/
+                $('.navbar li').on("click",function(){
+                    $('.navbar li a').removeClass('active');
+                    $(this).children("a").addClass('active');
+				
+					
+					
+					
+                
+                });
+			},//end link
+		}
+}]);
 
--- js/controllers/lipomatic-Controller.js
+++ js/controllers/lipomatic-Controller.js
@@ -5,39 +5,15 @@ angular.module('kiarash')
 .directive('fancyBox', function (){
 		return {
 			link: function($scope) {
-               
-                $('.fancybox-thumbs').fancybox({
-                    prevEffect : 'none',
-                    nextEffect : 'none',
-                    padding : 0 ,
-
-                    closeBtn  : true,
-                    arrows    : false,
-                    nextClick : true,
-
-                    helpers : {
-                        thumbs : {
-                            width  : 50,
-                            height : 50
-                        }
-                    },
-                     onUpdate:function(){
-                         /*========================================================*/
-                          $('.fancybox-overlay').hammer().on("swiperight", function(event) {
-                   
-                               $.fancybox.next();
-                              return false;
-                            });
-                            $('.fancybox-overlay').hammer().on("swipeleft", function(event) {
-                               $.fancybox.prev();
-                              
-                                 return false;
-                            });
-                            /*========================================================*/
-                     }
+                $('.icon ,.service_popup').click(function(){
+                    page = $(this).attr('page')
+                   $.fancybox.open({
+                        href : 'page/popupl/'+page+'.html',
+                        type : 'iframe',
+                        padding : 5
+                    });
                 });
 
-				
                 /*==========================================*/
                 
                  swiper2 = new Swiper('.swiper-container',{
@@ -62,6 +38,11 @@ angular.module('kiarash')
                    
                  });
                 /*==========================================*/
+				
+		
+		
+				
+				
 			},//end link
 		}
 });
